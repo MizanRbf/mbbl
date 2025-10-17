5451705d5c4d5494f9891c45df258a27d7ffd255
MizanRbf <mizanrbf@gmail.com>
2025-10-17 19:48:18 +0600

    miza's brian boost lab project initialized

---
+++ src/app/Components/Navbar/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center">
+      <img src="/assets/logo.png" alt="logo" className="w-20 h-20" />
+      <p className="text-5xl font-bold text-primary">MBBL</p>
+    </div>
+  );
+};
+
+export default Logo;

d1f8e845697bc4e5b9373438e5281e3bd575db2a
MizanRbf <mizanrbf@gmail.com>
2025-10-17 20:15:48 +0600

    navbar created

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -3,8 +3,10 @@ import Logo from "./Logo";
 
 const Navbar = () => {
   return (
-    <div className="border m-4 p-2">
+    <div className="m-4 p-2 flex justify-between items-center">
       <Logo></Logo>
+      <input type="text" className="border rounded-sm" />
+      <h2>Profile</h2>
     </div>
   );
 };

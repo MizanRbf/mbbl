ebf4a06a64e903f1824fe26ad6ee41b3847c44b4
MizanRbf <mizanrbf@gmail.com>
2025-10-18 08:50:45 +0600

    google font imported for styling logo font

--- src/app/Components/Navbar/Logo.jsx
+++ src/app/Components/Navbar/Logo.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <img src="/assets/logo.png" alt="logo" className="w-25" />
-      <p className="text-5xl font-bold text-primary">Mizan's Brain Boost Lab</p>
+      <img src="/assets/logo.png" alt="logo" className="lg:w-25 md:w-20 w-15" />
+      <p className="text-xl md:text-4xl lg:text-5xl font-bold text-primary font1">
+        Mizan's Brain Boost Lab
+      </p>
     </div>
   );
 };

585df0c7c120a47ea97dc7679fe744e09ee6d268
MizanRbf <mizanrbf@gmail.com>
2026-01-06 20:30:25 +0600

    logo and name updated

--- src/app/Components/Navbar/Logo.jsx
+++ src/app/Components/Navbar/Logo.jsx
@@ -5,9 +5,9 @@ import { Typewriter } from "react-simple-typewriter";
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <img src="/assets/logo.png" alt="logo" className="lg:w-25 md:w-20 w-15" />
+      <img src="/assets/logo.png" alt="logo" className="lg:w-22 md:w-20 w-15" />
       <p className="text-xl md:text-4xl lg:text-5xl font-bold text-primary font1">
-        <p className="font-bold text-lg md:text-2xl lg:text-4xl">
+        <p className="font-bold text-lg md:text-2xl lg:text-5xl">
           Mizan's Brain Boost Lab
         </p>
       </p>

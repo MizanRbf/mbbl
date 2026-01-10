4231b4f06e55008d67b8dc7e8e53a9603502913a
MizanRbf <mizanrbf@gmail.com>
2026-01-10 23:07:44 +0600

    books page title design updated

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="max-w-[1200px] mx-auto px-4">
-      <h1>Books</h1>
+    <div className="max-w-[1200px] mx-auto px-4 text-center pt-10">
+      {/* Title */}
+      <p className="text-4xl font-bold text-primary">Books</p>
+      {/* Books */}
+      <div>
+        <div className=""></div>
+      </div>
     </div>
   );
 };
